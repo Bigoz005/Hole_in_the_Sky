2126451ac1e4459d1b70bd398bcc13757b6686d9
MichałNawrot <michal.nawrot.mn.mn@gmail.com>
2020-05-19 23:48:38 +0200

    Damage effect fixed

--- Hole In The Sky/Assets/Scripts/PlayerHealth.cs
+++ Hole In The Sky/Assets/Scripts/PlayerHealth.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.Rendering.PostProcessing;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class PlayerHealth : MonoBehaviour
     private bool isTakingDmg = false;
     private bool gettingHit = false;
     private Camera playerCamera;
+    private FirstPersonController playerController;
     private ColorGrading colorGrading;
     private DepthOfField depthOfField;
 
@@ -20,6 +22,7 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHp = maxHp;
         playerCamera = GetComponentInChildren<Camera>();
+        playerController = GetComponent<FirstPersonController>();
         PostProcessVolume postProcessVolume = GetComponentInChildren<PostProcessVolume>();
         postProcessVolume.profile.TryGetSettings<ColorGrading>(out colorGrading);
         postProcessVolume.profile.TryGetSettings<DepthOfField>(out depthOfField);
@@ -33,59 +36,59 @@ public class PlayerHealth : MonoBehaviour
             if (currentHp < maxHp)
             {
                 currentHp += Time.deltaTime * 5;
-                Debug.Log("HP: " + currentHp);
             }
 
             if (playerCamera.fieldOfView > 63)
             {
-                playerCamera.fieldOfView -= Time.deltaTime;
-                Debug.Log("FOV: " + playerCamera.fieldOfView);
+                playerCamera.fieldOfView -= Time.deltaTime * 0.9f;
             }
 
             if (colorGrading.saturation.value < 15)
             {
-                colorGrading.saturation.value += Time.deltaTime * 10;
-                Debug.Log("SAT: " + colorGrading.saturation.value);
+                colorGrading.saturation.value += Time.deltaTime * 7.0f;
             }
 
             if (depthOfField.focusDistance.value < 7)
             {
-                depthOfField.focusDistance.value += Time.deltaTime * 1.5f;
-                Debug.Log("FOC: " + depthOfField.focusDistance.value);
+                depthOfField.focusDistance.value += Time.deltaTime * 0.9f;
+            }
+            
+            if (playerController.m_WalkSpeed < 2)
+            {
+                playerController.m_WalkSpeed += Time.deltaTime * 0.8f;
+            }
+
+            if (playerController.m_RunSpeed < 3)
+            {
+                playerController.m_RunSpeed += Time.deltaTime * 0.8f;
             }
         }
 
-        if (isTakingDmg)
+        if (gettingHit)
         {
-            if (gettingHit)
-            {
-                Camera.main.transform.position += Random.insideUnitSphere * 0.1f;
-            }
+            Camera.main.transform.position += Random.insideUnitSphere * 0.05f;
+        }
 
-            if (playerCamera.fieldOfView < 74)
-            {
-                playerCamera.fieldOfView += Time.deltaTime * 7;
-            }
-            if (colorGrading.saturation.value > -100)
-            {
-                colorGrading.saturation.value -= Time.deltaTime * 3;
-            }
+        if(currentHp <= 40)
+        {
+            Camera.main.transform.position += Random.insideUnitSphere * 0.04f;
+        }
 
-            if (depthOfField.focusDistance.value > 0)
-            {
-                depthOfField.focusDistance.value -= Time.deltaTime * 1.1f;
-            }
+        if (currentHp <= 20)
+        {
+            Camera.main.transform.position += Random.insideUnitSphere * 0.06f;
         }
     }
 
     public void TakeDamage(int _damage)
     {
         isHealing = false;
-        isTakingDmg = true;
-        gettingHit = true;
 
-        StartCoroutine("HitDuration");
+        StartCoroutine("WaitForHit");
+
         currentHp -= _damage;
+
+        CheckHP();
 
         if (currentHp <= 0)
         {
@@ -97,24 +100,87 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    public void CheckHP()
+    {
+        if(currentHp <= 100 && currentHp > 80)
+        {
+            playerCamera.fieldOfView = 63;
+            colorGrading.saturation.value = 15;
+            depthOfField.focusDistance.value = 7;
+            playerController.m_WalkSpeed = 2;
+            playerController.m_RunSpeed = 3;
+        }
+
+        if (currentHp <= 80 && currentHp > 60)
+        {
+            playerCamera.fieldOfView = 68;
+            colorGrading.saturation.value = -10;
+            depthOfField.focusDistance.value = 4;
+            playerController.m_WalkSpeed = 1.7f;
+            playerController.m_RunSpeed = 2.5f;
+        }
+
+        if (currentHp <= 60 && currentHp > 40)
+        {
+            playerCamera.fieldOfView = 73;
+            colorGrading.saturation.value = -35;
+            depthOfField.focusDistance.value = 1.5f;
+            playerController.m_WalkSpeed = 1.5f;
+            playerController.m_RunSpeed = 2.2f;
+        }
+
+        if (currentHp <= 40 && currentHp > 20)
+        {
+            playerCamera.fieldOfView = 78;
+            colorGrading.saturation.value = -60;
+            depthOfField.focusDistance.value = 0.8f;
+            playerController.m_WalkSpeed = 1.3f;
+            playerController.m_RunSpeed = 1.8f;
+        }
+
+        if (currentHp <= 20 && currentHp > 0)
+        {
+            playerCamera.fieldOfView = 83;
+            colorGrading.saturation.value = -85;
+            depthOfField.focusDistance.value = 0.4f;
+            playerController.m_WalkSpeed = 1.0f;
+            playerController.m_RunSpeed = 1.5f;
+        }
+    }
+
     public void Die()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
-        SceneManager.LoadScene("MainMenu");
+        StartCoroutine("WaitForDie");
     }
 
     public IEnumerator WaitForHealing()
     {
-        yield return new WaitForSecondsRealtime(10.0f);
+        yield return new WaitForSecondsRealtime(15.0f);
         isHealing = true;
-        isTakingDmg = false;
+    }
+
+    public IEnumerator WaitForHit()
+    {
+        yield return new WaitForSecondsRealtime(0.25f);
+        gettingHit = true;
+        StartCoroutine("HitDuration");
     }
 
     public IEnumerator HitDuration()
     {
-        yield return new WaitForSecondsRealtime(0.25f);
+        yield return new WaitForSecondsRealtime(0.4f);
         gettingHit = false;
     }
+
+    public IEnumerator WaitForDie()
+    {
+        yield return new WaitForSecondsRealtime(5.0f);
+        SceneManager.LoadScene("MainMenu");
+    }
+
 }
+
+
