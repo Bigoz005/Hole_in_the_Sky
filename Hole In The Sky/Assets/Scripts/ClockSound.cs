ad1ee4d490ebf150f542e669a0edaa01860846e8
MichałNawrot <michal.nawrot.mn.mn@gmail.com>
2020-06-01 20:26:38 +0200

    Added clocks for a padlock riddle

---
+++ Hole In The Sky/Assets/Scripts/ClockSound.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClockSound : MonoBehaviour
+{
+    public float clockSpeed = 1.0f;
+
+    int seconds;
+    float msecs;
+    private AudioSource audioSource;
+    public AudioClip secondsClip;
+    public AudioClip oddSecondsClip;
+
+    void Start()
+    {
+        msecs = 0.0f;
+        seconds = 0;
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    void Update()
+    {
+        msecs += Time.deltaTime * clockSpeed;
+        if (msecs >= 1.0f)
+        {
+            msecs -= 1.0f;
+            seconds++;
+            if (seconds % 2 == 0)
+            {
+                audioSource.PlayOneShot(secondsClip);
+            }
+            else
+            {
+                audioSource.PlayOneShot(oddSecondsClip);
+            }
+        }
+    }
+}
